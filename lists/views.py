9ab4894b03877b5dbf22768094949255792ee33d
Nichole Booker <nicholebooker93@gmail.com>
2026-02-26 14:09:29 -0500

    First unit test and view function

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+#Nichole Booker 2-26-2026
 
-# Create your views here.
+from django.http import HttpResponse
+
+def home_page(request):
+    return HttpResponse("<html><title>To-Do lists</title></html>")
