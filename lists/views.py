cf45b91fc5293eb27018a6551d21b69a9628338e
Nichole Booker <nicholebooker93@gmail.com>
2026-04-03 17:26:05 -0400

    Committing before refactoring the code

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,12 @@
-#Nichole Booker 3-11-2026
+#Nichole Booker 4-3-2026
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, "home.html")
-
+    return render(
+        request,
+        "home.html",
+        {"new_item_text": request.POST.get("item_text", "")},
+    )
